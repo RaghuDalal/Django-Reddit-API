bd9cbdaf49bc5bf43b60f3006c38a48f40827c5d
Raghvender <raghvender002@gmail.com>
2020-07-20 12:26:25 +0530

    Added Vote API

--- posts/views.py
+++ posts/views.py
@@ -13,3 +13,25 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)
+
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
+    serializer_class = VoteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user, post=post)
+
+    def perform_create(self, serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You have already voted for this post :)')
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('You never voted for this post...silly!')
